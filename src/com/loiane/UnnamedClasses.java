6b91e3cf3fe42132d54dad1565a6b2200ac2ff0d
Loiane <loianeg@gmail.com>
2023-10-28 17:11:01 -0400

    Commit message: Add UnnamedClasses class to illustrate basic Java concepts in Java 21
    This commit introduces a new file, UnnamedClasses.java, to the 'com.loiane' package. The file contains a basic main function that prints a "Hello" string to standard output. The purpose of this class is to serve as a basic introduction to Java for beginners and to lay foundations for further, more complex examples. No advanced features are utilized in this commit. Future commits will gradually introduce more complex coding scenarios and new Java features to encourage progressive learning.

---
+++ src/com/loiane/UnnamedClasses.java
@@ -0,0 +1,10 @@
+package com.loiane;
+
+public class UnnamedClasses {
+
+    void main() {
+        System.out.println("Hello");
+    }
+}
+
+// java --source 21 --enable-preview src/com/loiane/UnnamedClasses.java
